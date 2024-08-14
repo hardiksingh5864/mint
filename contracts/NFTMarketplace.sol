67df1e74e0b99f25c35bb958e0a0df7c7bb989df
hardik singh <hardikgamer2098@gmail.com>
2024-08-14 15:35:39 +0530

    Nft Changes

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -7,11 +7,11 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract NFTMarketplace is ERC721URIStorage {
     using Counters for Counters.Counter;
 
-    Counters.Counter private _tokenIDs;
+    Counters.Counter private _tokenIds;
     Counters.Counter private _itemsSold;
 
-    uint256 listingPrice = 0.025 ether;
-    address payable owner;
+    uint256 public listingPrice = 0.025 ether;
+    address payable public owner;
 
     mapping(uint256 => MarketItem) private idToMarketItem;
 
@@ -22,6 +22,7 @@ contract NFTMarketplace is ERC721URIStorage {
         uint256 price;
         bool sold;
     }
+
     event MarketItemCreated(
         uint256 indexed tokenId,
         address seller,
@@ -30,13 +31,120 @@ contract NFTMarketplace is ERC721URIStorage {
         bool sold
     );
 
-    constructor() {
+    constructor() ERC721("NFTMarketplace", "NFTM") {
         owner = payable(msg.sender);
     }
-    function updateListingPrice(uint _listingPrice) public payable {
+
+    function updateListingPrice(uint256 _listingPrice) public {
         require(
             msg.sender == owner,
             "Only the owner can update the listing price"
         );
+        listingPrice = _listingPrice;
+    }
+
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    function createToken(
+        string memory tokenURI,
+        uint256 price
+    ) public payable returns (uint256) {
+        require(price > 0, "Price must be greater than zero");
+        require(
+            msg.value == listingPrice,
+            "Price must be equal to listing price"
+        );
+
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        createMarketItem(newTokenId, price);
+        return newTokenId;
+    }
+
+    function createMarketItem(uint256 tokenId, uint256 price) private {
+        require(price > 0, "Price must be at least 1");
+
+        idToMarketItem[tokenId] = MarketItem(
+            tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            price,
+            false
+        );
+        _transfer(msg.sender, address(this), tokenId);
+
+        emit MarketItemCreated(
+            tokenId,
+            msg.sender,
+            address(this),
+            price,
+            false
+        );
+    }
+
+    function resellToken(uint256 tokenId, uint256 price) public payable {
+        require(
+            idToMarketItem[tokenId].owner == msg.sender,
+            "Only the item owner can perform this operation"
+        );
+        require(
+            msg.value == listingPrice,
+            "Price must be equal to listing price"
+        );
+
+        idToMarketItem[tokenId].sold = false;
+        idToMarketItem[tokenId].price = price;
+        idToMarketItem[tokenId].seller = payable(msg.sender);
+        idToMarketItem[tokenId].owner = payable(address(this));
+
+        _itemsSold.decrement();
+
+        _transfer(msg.sender, address(this), tokenId);
+    }
+
+    function createMarketSale(uint256 tokenId) public payable {
+        uint256 price = idToMarketItem[tokenId].price;
+        address seller = idToMarketItem[tokenId].seller;
+
+        require(
+            msg.value == price,
+            "Please submit the asking price in order to complete the purchase"
+        );
+
+        idToMarketItem[tokenId].owner = payable(msg.sender);
+        idToMarketItem[tokenId].sold = true;
+        idToMarketItem[tokenId].seller = payable(address(0));
+
+        _itemsSold.increment();
+
+        _transfer(address(this), msg.sender, tokenId);
+
+        payable(owner).transfer(listingPrice);
+        payable(idToMarketItem[tokenId].seller).transfer(msg.value);
+    }
+
+    function fetchMarketItems() public view returns (MarketItem[] memory) {
+        uint256 itemCount = _tokenIds.current();
+        uint256 unSoldItemCount = _tokenIds.current() - _itemsSold.current();
+        uint256 currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unSoldItemCount);
+
+        for (uint i = 0; i < itemCount; i++) {
+            if (idToMarketItem[i + 1].owner == address(this)) {
+                uint currentId = i + 1;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
     }
 }
