0e4abef18565694d0855e9ff58cdd72525191c1d
hardik singh <hardikgamer2098@gmail.com>
2024-08-18 03:07:58 +0530

    some addition for smart contracts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ethers: "readonly",
+        process: "readonly",
+        require: "readonly",
+        module: "readonly",
+      },
+    },
+  },
   pluginJs.configs.recommended,
   pluginReactConfig,
 ];
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,7 @@
+require('@nomiclabs/hardhat-ethers');
+const fs = require('fs'); // Corrected require statement
+
+const privateKey = fs.readFileSync('.secret').toString().trim(); // Corrected toString() method
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -6,5 +10,18 @@ module.exports = {
     hardhat: {
       chainId: 31337, // Default Chain ID for Hardhat
     },
+    localhost: {
+      url: "http://127.0.0.1:8545", // URL for local Hardhat network
+    },
+    // Add additional network configurations if needed
+    // e.g., Rinkeby or Mainnet
+    // rinkeby: {
+    //   url: `https://rinkeby.infura.io/v3/YOUR_INFURA_PROJECT_ID`,
+    //   accounts: [privateKey],
+    // },
+    // mainnet: {
+    //   url: `https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID`,
+    //   accounts: [privateKey],
+    // },
   },
 };
